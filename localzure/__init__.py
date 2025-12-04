39ef3a239eb4f1ea881b6b2af12bf05fd92181da
Ayodele Oladeji <ayodele_oladeji@firstderivative.com>
2025-12-04 15:57:02 +0000

    Initial barebones

---
+++ localzure/__init__.py
@@ -0,0 +1,12 @@
+"""
+LocalZure: Local Azure Cloud Platform Emulator
+
+A fully local Azure cloud platform emulator for offline development and testing.
+"""
+
+__version__ = "0.1.0"
+__author__ = "LocalZure Contributors"
+
+from .core.runtime import LocalZureRuntime
+
+__all__ = ["LocalZureRuntime", "__version__"]
